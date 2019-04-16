18f75a4843674505a013ed6870f23a86e9f7ffe8
Niklas Johansson <niklas@hjuvik.net>
2019-04-16 21:45:25 +0200

    lesson 7

--- app-swosh/app-swosh/Controller/LeaugeVC.swift
+++ app-swosh/app-swosh/Controller/LeaugeVC.swift
@@ -9,22 +9,48 @@
 import UIKit
 
 class LeaugeVC: UIViewController {
+
+    var player: Player!
     
+    @IBOutlet weak var nextBtn: BorderButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
-    @IBAction func unwindFromSkillVC(unwindSegue: UIStoryboardSegue) {
+        player = Player()
         
     }
     
-    @IBAction func onNextTapped(_ sender: Any) {
+     @IBAction func onNextTapped(_ sender: Any) {
         performSegue(withIdentifier: "skillVCSegue", sender: self)
     }
     
-
+    @IBAction func onMensTapped(_ sender: Any) {
+        selectLeague(leagueType: "mens")
     
+    }
+    
+    @IBAction func onWomensTapped(_ sender: Any) {
+        selectLeague(leagueType: "womens")
+        
+    }
+    
+    @IBAction func onCoedTapped(_ sender: Any) {
+        selectLeague(leagueType: "coed")
+        
+    }
+    
+    func selectLeague(leagueType: String) {
+        player.desiredLeague = leagueType
+        nextBtn.isEnabled = true
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let skillVC = segue.destination as? SkillVC {
+            
+            skillVC.player = player
+        }
+    }
     /*
     // MARK: - Navigation
 
---
+++ app-swosh/app-swosh/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  app-swosh
+//
+//  Created by Niklas Johansson on 07-04-2019.
+//  Copyright © 2019 Anaconda. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String!
+    var selectedSkillLevel: String!
+}
